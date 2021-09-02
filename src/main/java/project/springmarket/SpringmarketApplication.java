c242fa9c3d0ee975808ed11eb42b9cb137d10cf4
wongi.lee <wongi.lee@linecorp.com>
2021-09-03 01:46:03 +0900

    Add scanBasePackages in SpringmarketApplication.class

--- src/main/java/project/springmarket/SpringmarketApplication.java
+++ src/main/java/project/springmarket/SpringmarketApplication.java
@@ -3,7 +3,7 @@ package project.springmarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "project.springmarket")
 public class SpringmarketApplication {
 
     public static void main(String[] args) {
