e9477e46e2959060a679d16929d5f01e607a16b7
mankicho <skxz123@gmail.com>
2021-09-02 20:38:53 +0900

    project environment setting

---
+++ src/main/java/project/springmarket/SpringmarketApplication.java
@@ -0,0 +1,13 @@
+package project.springmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmarketApplication.class, args);
+    }
+
+}
