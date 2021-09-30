ff3f4550ca73bfbcbccda37dbe6583191bd1d85a
mankicho <skxz123@gmail.com>
2021-09-30 20:17:39 +0900

    add validation to product model

--- src/main/java/project/springmarket/model/product/ProductVO.java
+++ src/main/java/project/springmarket/model/product/ProductVO.java
@@ -4,16 +4,25 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Builder
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class ProductVO {
     private int productNo;
     private int sellerNo;
     private int buyerNo;
+
+    @NotNull
+    @NotEmpty(message = "product's name must not empty")
     private String productName;
+
+    @NotNull
+    @NotEmpty(message = "description must not empty")
     private String description;
-    private ProductStatus productStatus;
+
+    private ProductStatus productStatus ;
     private String ProductImages;
     private int category;
     private int addressNo;
