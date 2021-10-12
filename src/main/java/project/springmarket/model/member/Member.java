901fccd7e9d9accc5e0b1fb50e8130ee3293dc20
bbeomgeun <miki308@naver.com>
2021-10-12 16:40:11 +0900

    Add setter to member

--- src/main/java/project/springmarket/model/member/Member.java
+++ src/main/java/project/springmarket/model/member/Member.java
@@ -2,9 +2,11 @@ package project.springmarket.model.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
 @AllArgsConstructor
 public class Member {
